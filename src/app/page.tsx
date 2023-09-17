af6c6d993db831006d5a7d27689dc59fc2c794c8
MinhMatMong <kurominhnhat@gmail.com>
2023-09-17 20:50:16 +0700

    test og in main page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Hub Media - Connect Your Brand To The World',
+    openGraph: {
+        images: ['/backlog.png'],
+    },
+    description: 'Connect Your Brand To The World',
+};
+
+export default function Page() {}
