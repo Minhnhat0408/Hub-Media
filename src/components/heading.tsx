2c0646f4a08e7273621f000d2651e9deca2e1943
MinhMatMong <kurominhnhat@gmail.com>
2023-09-26 00:44:36 +0700

    fix cors and animation bugs

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -16,14 +16,16 @@ function Heading({ title, description,className }: { title: string; description:
 
             <div className={cn(' h-fit  relative flex text-6xl justify-center  font-bold ')}>
                 {description.split(' ').map((word, index) => {
-                    let time = index <1 ? 80: index*80*description.split(' ')[index-1].length;
+                    let time = index <1 ? 80: 80*description.split(' ').splice(0,index).reduce((a,b)=>{
+                        return a + b.length
+                    },0);
                     return (
                         <span key={index} className="mr-4 flex">
                             {word.split('').map((char, ind) => {
                                 time += 80;
                                 return (
                                     <span
-                                        className={cn("opacity-0 pt-2 translate-x-[50px] scale-150 duration-700  " ,isInView && ' flex opacity-1 translate-x-0  super scale-100')}
+                                        className={cn("opacity-0 py-2 translate-x-[50px] scale-150 duration-700  " ,isInView && ' flex opacity-1 translate-x-0  super scale-100')}
                                         style={{ transitionDelay: time  + 'ms' }}
                                         key={ind}
                                     >
