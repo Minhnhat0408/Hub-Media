1ed8a08b57f7edb0ae2def1dab595b79bd15f72b
MinhMatMong <kurominhnhat@gmail.com>
2023-11-15 00:00:25 +0700

    git fix loading imag

--- src/app/[lang]/(hubmedia)/loading.tsx
+++ src/app/[lang]/(hubmedia)/loading.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Loading() {
     return ( 
         <main className="w-[100vw] h-[100vh] fixed z-[200] flex flex-col justify-center items-center bg-background">
-            <Image src={'/images/logo.png'} alt="img" width="0" height="0" sizes="100vw" className=" h-auto  xl:!w-[15vw] md:!w-[20vw] ssm:w-[25vw] w-[30vw]    animate-pulse" />
+            <Image src={'/logo/logo.png'} alt="img" width="0" height="0" sizes="100vw" className=" h-auto  xl:!w-[15vw] md:!w-[20vw] ssm:w-[25vw] w-[30vw]    animate-pulse" />
         </main>
      );
 }
